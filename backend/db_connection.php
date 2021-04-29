128647a091bb38737f790eb6e9fb8470ed16df36
Julia Henrich <inf2671@hs-worms.de>
2021-04-29 15:02:55 +0200

    first version db connection

---
+++ backend/db_connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "127.0.0.1:3306";
+$username = "root";
+$password = "password";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
