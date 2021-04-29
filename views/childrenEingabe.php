202d0fa4f93c4351971f8b5bba02f5c406067497
Ismet223 <Ismet.Yueksel@gmx.de>
2021-04-29 19:23:07 +0200

    kleine Anpassungen

--- views/childrenEingabe.php
+++ views/childrenEingabe.php
@@ -1,12 +1,6 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="/kipa/libs/bootstrap-5.0.0/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
 
     <title>Children</title>
 
@@ -103,8 +97,6 @@
     </form>
  
 
-    <script src="/kipa/libs/bootstrap-5.0.0/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-
     <script>
 
         // Example starter JavaScript for disabling form submissions if there are invalid fields
@@ -129,11 +121,5 @@
         })()
 
     </script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
-    -->
   </body>
 </html>
