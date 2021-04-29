eff858f01946acb09b5670bb86ccec62fa8b3d84
Ismet223 <Ismet.Yueksel@gmx.de>
2021-04-29 16:54:22 +0200

    Background Upload

--- html2/MedicalTab.php
+++ html2/MedicalTab.php
@@ -11,7 +11,7 @@
 
     <title>Medical Tab</title>
   </head>
-  <body class="bg-secondary">
+  <body>
     <div class="container">
         <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
         <li class="nav-item" role="presentation">
@@ -47,5 +47,11 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
     -->
+
+    <style>
+      body {
+        background-image: url('Kinder-Paradise-Logo-web-orange.png');
+      }
+  </style>
   </body>
 </html>
