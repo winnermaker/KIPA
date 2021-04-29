1ff69cc0ed26aaf08499ca44d1d9b4ea8e3cdfbb
Julia Henrich <inf2671@hs-worms.de>
2021-04-29 19:36:55 +0200

    remove errors

--- html/childrenEingabe.php
+++ html/childrenEingabe.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "admin";
 $password = "admin";
-$databasename = "kipa"
+$databasename = "kipa";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $databasename);
