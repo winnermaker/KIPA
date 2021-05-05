b25243b60c88a8eb2be38e1bbc0c9621da4a5c51
Ismet223 <Ismet.Yueksel@gmx.de>
2021-05-05 16:06:14 +0200

    kleine Anpassung

--- views/inputForm.php
+++ views/inputForm.php
@@ -13,7 +13,7 @@
   </head>
   <body>
     <div class="container">
-        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+        <ul class="nav nav-pills mb-3 mt-3" id="pills-tab" role="tablist">
         <li class="nav-item" role="presentation">
         <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Children</button>
       </li>
