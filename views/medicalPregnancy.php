6be4c3fdae44373457bab6a3d1f45e69b52410db
Ismet223 <Ismet.Yueksel@gmx.de>
2021-05-18 10:18:44 +0200

    pregnancy add children hinzugefügt

--- views/medicalPregnancy.php
+++ views/medicalPregnancy.php
@@ -89,87 +89,87 @@
 
             <h3 class="mt-5">Previous Pregnancies</h3>
 
+            <div id="childs">
+                <div class="row mt-4">
+                    <fieldset class="col-3">
+                    <legend class="col-form-label">Gender</legend>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="genderpregnancyRadios" id="malepregnancyRadio" value="option1">
+                            <label class="form-check-label" for="malepregnancyRadio">Male</label>
+                        </div>
 
-            <div class="row mt-4">
-                <fieldset class="col-3">
-                <legend class="col-form-label">Gender</legend>
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="genderpregnancyRadios" id="malepregnancyRadio" value="option1">
-                        <label class="form-check-label" for="malepregnancyRadio">Male</label>
-                    </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="genderpregnancyRadios" id="femalepregnancyRadio" value="option2">
+                            <label class="form-check-label" for="femalepregnancyRadio">Female</label>
+                        </div>
 
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="genderpregnancyRadios" id="femalepregnancyRadio" value="option2">
-                        <label class="form-check-label" for="femalepregnancyRadio">Female</label>
-                    </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="genderpregnancyRadios" id="otherpregnancyRadio" value="option2">
+                            <label class="form-check-label" for="otherpregnancyRadio">Other</label>
+                        </div>
+                    </fieldset>
 
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="genderpregnancyRadios" id="otherpregnancyRadio" value="option2">
-                        <label class="form-check-label" for="otherpregnancyRadio">Other</label>
-                    </div>
-                </fieldset>
+                    
+                        <div class="form-group col">
+                            <label class="form-label" for="Name">Name</label>
+                            <input type="text" class="form-control" placeholder="Max" id="Name">
+                        </div>
 
-                
+                        <div class ="form-group col-3">
+                            <label for="dateofBirth" class="form-label">Date of Birth</label>
+                            <input class="form-control " type="text"  id="dateofBirth">        
+                        </div>
+
+                </div>
+
+                <div class="row mt-3">
                     <div class="form-group col">
-                        <label class="form-label" for="Name">Name</label>
-                        <input type="text" class="form-control" placeholder="Max" id="Name">
+                        <label for="eventsDuringPregnancy" class="form-label">Events during pregnancy ?</label>
+                        <textarea class="form-control" id="eventsDuringPregnancy" rows="2"></textarea>
                     </div>
 
-                    <div class ="form-group col-3">
-                        <label for="dateofBirth" class="form-label">Date of Birth</label>
-                        <input class="form-control " type="text"  id="dateofBirth">        
+                    <div class="form-group col">
+                        <label for="durationOfLabor" class="form-label">Duration of labor ?</label>
+                        <textarea class="form-control" id="durationOfLabor" rows="2"></textarea>
                     </div>
-
-            </div>
-
-            <div class="row mt-3">
-                <div class="form-group col">
-                    <label for="eventsDuringPregnancy" class="form-label">Events during pregnancy ?</label>
-                    <textarea class="form-control" id="eventsDuringPregnancy" rows="2"></textarea>
                 </div>
 
-                <div class="form-group col">
-                    <label for="durationOfLabor" class="form-label">Duration of labor ?</label>
-                    <textarea class="form-control" id="durationOfLabor" rows="2"></textarea>
-                </div>
-            </div>
-
-            <div class="row mt-3">
-                <div class="form-group col">
-                    <label for="spont_CS_forceps" class="form-label">Spontaneous/CS/forceps ?</label>
-                    <textarea class="form-control" id="spont_CS_forceps" rows="2"></textarea>
-                </div>
-            </div>
-
-            <div class="row mt-3">
-                <fieldset class="col-2">
-                <legend class="col-form-label">Healthy?</legend>
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="healthyRadios" id="healthyRadioYes" value="option1">
-                        <label class="form-check-label" for="healthyRadioYes">Yes</label>
+                <div class="row mt-3">
+                    <div class="form-group col">
+                        <label for="spont_CS_forceps" class="form-label">Spontaneous/CS/forceps ?</label>
+                        <textarea class="form-control" id="spont_CS_forceps" rows="2"></textarea>
                     </div>
+                </div>
 
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="healthyRadios" id="healthyRadioNo" value="option2">
-                        <label class="form-check-label" for="healthyRadioNo">No</label>
+                <div class="row mt-3">
+                    <fieldset class="col-2">
+                    <legend class="col-form-label">Healthy?</legend>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="healthyRadios" id="healthyRadioYes" value="option1">
+                            <label class="form-check-label" for="healthyRadioYes">Yes</label>
+                        </div>
+
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="healthyRadios" id="healthyRadioNo" value="option2">
+                            <label class="form-check-label" for="healthyRadioNo">No</label>
+                        </div>
+                    </fieldset>
+
+                    <div class="form-group col">
+                        <label for="childrenProblems" class="form-label">Problems</label>
+                        <textarea class="form-control" id="childrenProblems" rows="2"></textarea>
                     </div>
-                </fieldset>
-
-                <div class="form-group col">
-                    <label for="childrenProblems" class="form-label">Problems</label>
-                    <textarea class="form-control" id="childrenProblems" rows="2"></textarea>
                 </div>
-            </div>
 
-            <div class="row mt-3">
-                <div class="form-group col">
-                    <label for="Remarks" class="form-label">Remarks</label>
-                    <textarea class="form-control" id="Remarks" rows="4"></textarea>
+                <div class="row mt-3">
+                    <div class="form-group col">
+                        <label for="Remarks" class="form-label">Remarks</label>
+                        <textarea class="form-control" id="Remarks" rows="4"></textarea>
+                    </div>
                 </div>
-            </div>
 
-            <div class="form-row">
-                <button type="button" class="btn btn-primary col-1 mt-4">Add a new children</button>
+                <button type="button" onClick="duplicate()" class="btn btn-secondary mt-4">Add a new children</button>
+                
             </div>
 
 
@@ -177,6 +177,18 @@
 
         </form>
     </div>
+
+    <script>
+        var i = 0;
+        var original = document.getElementById('childs');
+
+        function duplicate() {
+            var clone = original.cloneNode(true); // "deep" clone
+            clone.id = "childs" + ++i;
+            // or clone.id = ""; if the divs don't need an ID
+            original.parentNode.appendChild(clone);
+}
+    </script>
 
   </body>
 </html>
--- views/test.php
+++ views/test.php
@@ -1,53 +1,20 @@
-<?php  require_once $_SERVER['DOCUMENT_ROOT'] . "/kipa/libary.html"; ?>
+<?php 
+    require 'navbar.php';
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/kipa/libary.html"; 
+    
+    ?>
 
-<html>
-<head>
-    <link href="css/bootstrap.css" rel="stylesheet">
-</head>
-<body>
-<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered">
-<tbody>
-    <tr class="rowlink">
-        <td>
-            <div class="dropdown">
-                <a class="dropdown-toggle rowlink" data-toggle="dropdown" href="#">
-                    Click For Dropdown Menu
-                </a>
-                <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-                    <li>
-                        <a tabindex="-1" href="#">
-                            Action
-                        </a>
-                    </li>
-                    <li>
-                        <a tabindex="-1" href="#">
-                            Another action
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </td>
-        <td>
-            Click For Dropdown Menu
-        </td>    
-    </tr>
-    <tr><td>Cell</td><td>Cell</td></tr>         
-</tbody>
-</table>​
-
-<script src="js/jquery.js"></script>
-<script src="js/bootstrap-dropdown.js"></script>
-
-<script>
-    $(function() {
-        $(this).find('.dropdown-toggle').dropdown();
-
-        $('.rowlink').on('click', function(e) {
-            $(this).find('.dropdown').toggleClass('open');
-            e.stopPropagation();
-        });
-    });
-</script>
-
-</body>
-</html>
+<ul class="nav nav-pills">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="patient.php">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
