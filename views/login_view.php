8bb58237b70dcd61d25a2160f5d8795d5d821919
Ismet223 <Ismet.Yueksel@gmx.de>
2021-06-10 08:19:16 +0200

    login_view image hinzugefügt

--- views/login_view.php
+++ views/login_view.php
@@ -1,7 +1,6 @@
 <!doctype html>
 <html lang="en">
   <head>
-
     <?php
          require_once $_SERVER['DOCUMENT_ROOT'] . "/kipa/libary.html";
          require_once $_SERVER['DOCUMENT_ROOT'] . "/kipa/views/navbar.php";
@@ -12,25 +11,32 @@
   </head>
   <body>
     <form action="login_con.php" method="post">
-        <div class="d-flex align-items-center justify-content-center mt-5">
-            <div class="container">
-                <img src="/kipa/views/img/med.png" alt="" width="250" height="100" >
+        <div class="">
+            <div class="container mt-5">
 
-                <div class="row">
-                    <div class="form-group mt-5 col-6">
+               
+                    <img src="/kipa/views/img/loginImg.png" alt="" width="350" height="150" >
+                    <h2 class="mt-3 col">Login</h2>
+
+                
+
+                <div class="form-row">
+                    <div class="form-group mt-5 col-4">
                         <label for="username" class="form-label">Username</label>
                         <input class="form-control" type="text" id="username" name="username">
                     </div>
                 </div>
 
-                <div class="row">
-                    <div class="form-group mt-3 col-6">
+                <div class="form-row">
+                    <div class="form-group mt-3 col-4">
                         <label for="password" class="form-label">Password</label>
                         <input class="form-control" type="password" name="password" id="password">
                     </div>
                 </div>
 
-                <button class="btn btn-primary mt-4" type="submit">Login</button>
+                <div class="form-row mt-4 ">
+                    <button class="btn btn-primary col-4" type="submit">Login</button>
+                </div>
 
             </div>
 
